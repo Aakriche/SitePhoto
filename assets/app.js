aa0f73e4d8b6376d8094c4d6b9c9524c73195373
Amaury Akriche <amaury@akriche.com>
2021-08-16 13:14:54 +0200

    ajout homepage

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.scss in this case)
+// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
 // You can specify which plugins you need
@@ -13,3 +13,4 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
+
