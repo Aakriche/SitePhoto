f96f0e5a37fca3d5b773919811de127b29ced5ea
Amaury Akriche <amaury@akriche.com>
2021-08-18 16:34:04 +0200

    ajout service envoi de mail

---
+++ src/Command/SendContactCommand.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Command;
+
+
+use App\Repository\ContactRepository;
+use App\Repository\UserRepository;
+use App\Service\ContactService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+
+class SendContactCommand extends Command
+{
+    private $contactRepository;
+    private $mailer;
+    private $contactService;
+    private $userRepository;
+    protected static $defaultName = 'app:send-contact';
+
+    public function __construct(
+        ContactRepository $contactRepository,
+        MailerInterface $mailer,
+        ContactService $contactService,
+        UserRepository $userRepository
+    ) {
+        $this->contactRepository = $contactRepository;
+        $this->mailer = $mailer;
+        $this->contactService = $contactService;
+        $this->userRepository = $userRepository;
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $toSend = $this->contactRepository->findBy(['isSent' => false]);
+        $address = new Address($this->userRepository->getPeintre()->getEmail(), $this->userRepository->getPeintre()->getNom() . ' ' . $this->userRepository->getPeintre()->getPrenom());
+
+        foreach ($toSend as $mail) {
+            $email = (new Email())
+                ->from($mail->getEmail())
+                ->to($address)
+                ->subject('Nouveau message de ' . $mail->getNom())
+                ->text($mail->getMessage());
+
+            $this->mailer->send($email);
+            $this->contactService->isSent($mail);
+        }
+        return Command::SUCCESS;
+    }
+
+
+}
---
+++ src/Service/ContactService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Service;
+
+
+use DateTime;
+use App\Entity\Contact;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+
+class ContactService
+{
+    private $manager;
+    private $flash;
+
+    public function __construct(EntityManagerInterface $manager, FlashBagInterface $flash)
+    {
+        $this->manager = $manager;
+        $this->flash = $flash;
+    }
+
+    public function persistContact(Contact $contact): void
+    {
+        $contact->setIsSent(false)
+            ->setCreatedAt(new DateTime('now'));
+
+        $this->manager->persist($contact);
+        $this->manager->flush();
+        $this->flash->add('success', 'Votre message est bien envoyé, merci');
+
+    }
+
+    public function isSent(Contact $contact): void{
+        $contact->setIsSent(true);
+
+        $this->manager->persist($contact);
+        $this->manager->flush();
+    }
+}
